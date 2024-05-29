21b748e94b059cd0fc1c4871ec08ec94d18baf47
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2024-05-29 13:16:27 -0300

    server alterations

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const app = require('./app')
 require('dotenv').config()
 
 
-const PORT = process.env.PORT  ? Number(process.env.PORT) : 3333
+const PORT = process.env.PORT || 3333
 
 app.listen(PORT, () =>{
     console.log(`Server running or port ${PORT}`)
