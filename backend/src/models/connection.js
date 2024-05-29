3e56a66f2424027b045d757f2a984894226c1aaf
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2024-05-29 14:23:26 -0300

    add

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -6,7 +6,11 @@ const connection = mysql.createPool({
     host: process.env.MYSQL_SERVER,
     user: process.env.MYSQL_USER ,
     password: process.env.MYSQL_PASSWORD ,
-    database:  process.env.MYSQL_DATABASE
+    database:  process.env.MYSQL_DATABASE,
+    connectTimeout: 10000, 
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
 
 
